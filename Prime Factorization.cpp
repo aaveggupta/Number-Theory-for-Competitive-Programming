e008f2728aec1e6a84fb69ef21925bb67c1abf9f
Aaveg Gupta <55689811+aaveggupta@users.noreply.github.com>
2020-10-26 04:04:28 +0530

    Create Prime Factorization.cpp

---
+++ Prime Factorization.cpp
@@ -0,0 +1,45 @@
+/********************
+Author -> Aaveg Gupta
+*********************/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+typedef long long ll;
+#define int ll
+ 
+ 
+// Prime Factorization -> decomposition of a composite number into a product of smaller prime numbers
+// eg N = 100
+// Factorization = (2^2) * (5^2)
+
+// if n is a composite number then there is atleast a one prime divisor of N below sqrt(N);
+
+void primeFact (int n) {
+  
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0) {
+      int cnt = 0;
+      while (n % i == 0) {
+        cnt ++;
+        n /= i;
+      }
+      cout << i << "^" << cnt << "\n";
+    }
+  }
+  
+  if (n > 1) {
+    cout << n << "^" << 1 << "\n";
+  }
+  
+}
+
+signed main()
+{
+  int n;
+  cin >> n;
+  
+  primeFact(n);
+    
+}
